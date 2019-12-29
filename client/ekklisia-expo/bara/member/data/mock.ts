d3aaf764e9cb9e830bfecf1f18193b05e9d311d7
nampdn <nampdn@protonmail.com>
2019-12-29 20:56:57 +0700

    chore(expo): add mock

--- client/ekklisia-expo/bara/member/data/mock.ts
+++ client/ekklisia-expo/bara/member/data/mock.ts
@@ -14,6 +14,7 @@ export const memberList = [
   },
   {
     name: 'Lê Vũ Văn Phi',
-    photoUrl: 'https://avatars1.githubusercontent.com/u/26531658?s=460&v=4',
+    photoUrl:
+      'https://scontent.fhph1-1.fna.fbcdn.net/v/t1.0-1/p200x200/69675813_2296042360517068_321716964848304128_o.jpg?_nc_cat=110&_nc_ohc=0ViFnIaBmH0AQkwt9SUfZqBpi9ChbH-Hs9SbAEqTbCS73E6dSidPguSkQ&_nc_ht=scontent.fhph1-1.fna&oh=ac588ea689007db09cc645f0f1d5b524&oe=5EA5C834',
   },
 ]
