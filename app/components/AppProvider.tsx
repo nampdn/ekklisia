60734ce00291080bc78c89f68c3f1ee596ddd709
nampdn <nampdn@protonmail.com>
2020-02-06 22:38:08 +0700

    feat(components): AppProvider add graphql service

--- app/components/AppProvider.tsx
+++ app/components/AppProvider.tsx
@@ -8,21 +8,34 @@ import { EvaIconsPack } from '@ui-kitten/eva-icons'
 import 'moment'
 import 'moment/locale/vi'
 import { BaraProvider, context } from '@bit/barajs.portions.react'
-import { BaraApp } from '../features'
+
+import { ApolloClient } from 'apollo-client'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
 
 import { run } from '@barajs/core'
+import { BaraApp } from '../features'
 import { configureStore } from '../features/redux'
 
 const store = configureStore()
 const bara = run(BaraApp(context, store))
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'https://graph.btngiadinh.com',
+  }),
+})
 
 export const AppProvider = ({ children }: any) => {
   return (
     <BaraProvider bara={bara} store={store}>
-      <IconRegistry icons={EvaIconsPack} />
-      <KittenProvider theme={darkTheme} mapping={mapping}>
-        {children}
-      </KittenProvider>
+      <ApolloProvider client={client}>
+        <IconRegistry icons={EvaIconsPack} />
+        <KittenProvider theme={darkTheme} mapping={mapping}>
+          {children}
+        </KittenProvider>
+      </ApolloProvider>
     </BaraProvider>
   )
 }
