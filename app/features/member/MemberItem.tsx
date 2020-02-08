c44cbc15e745afef6338a09db446d5cd823217b4
nampdn <nampdn@protonmail.com>
2020-02-08 23:39:22 +0700

    feat(member): allow press on entire member item

--- app/features/member/MemberItem.tsx
+++ app/features/member/MemberItem.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import { StyleSheet } from 'react-native'
-import { Avatar, Text, Layout, Toggle, CheckBox } from '@ui-kitten/components'
+import { StyleSheet, TouchableOpacity } from 'react-native'
+import { Avatar, Text, Layout, CheckBox } from '@ui-kitten/components'
 
 const styles = StyleSheet.create({
   memberItem: {
@@ -31,20 +31,27 @@ export const MemberItem = ({ member }: MemberItemProps) => {
     setChecked(!checked)
   }
   return (
-    <Layout level="3" style={styles.memberItem}>
-      <Avatar
-        style={styles.avatar}
-        size="large"
-        source={{ uri: member.photoUrl }}
-      />
-      <Text style={styles.text} status="basic" category="s1" numberOfLines={1}>
-        {member.name}
-      </Text>
-      <CheckBox
-        style={styles.toggle}
-        checked={checked}
-        onChange={switchCheck}
-      />
-    </Layout>
+    <TouchableOpacity onPress={switchCheck}>
+      <Layout level="3" style={styles.memberItem}>
+        <Avatar
+          style={styles.avatar}
+          size="large"
+          source={{ uri: member.photoUrl }}
+        />
+        <Text
+          style={styles.text}
+          status="basic"
+          category="s1"
+          numberOfLines={1}
+        >
+          {member.name}
+        </Text>
+        <CheckBox
+          style={styles.toggle}
+          checked={checked}
+          onChange={switchCheck}
+        />
+      </Layout>
+    </TouchableOpacity>
   )
 }
